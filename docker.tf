628254f03947d02dc03e02b656f4db83f7492112
kalyantalbothula <kalyan6166@gmail.com>
2025-09-11 08:38:57 +1000

    updating

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "docker" {
   ami           = local.ami_id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   #instance_type = "t3.medium"
   # need more for terraform
